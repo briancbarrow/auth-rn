810fbf8b2fa5d0b43f43771ea2e1452875c02041
Brian Barrow <briancbarrow@gmail.com>
2019-01-17 06:13:44 -0700

    added spinner and log in logic

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,12 +1,36 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { CardSection, Button, Card, AuthText } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { CardSection, Button, Card, AuthText, Spinner } from './common';
 
 class Auth extends Component {
 state = {
     email: '',
-    password: ''
+    password: '',
+    signInError: '',
+    loggingIn: false
 };
+
+onButtonPress() {
+    this.setState({ signInError: '', loggingIn: true })
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+                .catch(() => {
+                    this.setState({ signInError: 'Authentication Failed' });
+                });
+        });
+};
+
+renderButton() {
+    if(this.state.loggingIn) {
+        return <Spinner size="small" />;
+    }
+    return (
+        <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
+    );
+}
 
     render() {
         return (
@@ -29,11 +53,23 @@ state = {
                     />
                 </CardSection>
 
+                <Text style={styles.errorTextStyle}>
+                    {this.state.signInError}
+                </Text>
+
                 <CardSection>
-                    <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
+    }
+}
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
     }
 }
 
